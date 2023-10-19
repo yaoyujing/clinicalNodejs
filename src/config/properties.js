726c2d6d416105615c1c7b37504bdc83fa4b2f05
yujing <yujing>
2023-10-20 01:57:05 +0800

    update code

--- src/config/properties.js
+++ src/config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT:8000,
-    DB:'mongodb://localhost:27017/clinicals'
+    DB:"mongodb+srv://yujing:nomekop1oo@clinicalnode.s5j1gd3.mongodb.net/?retryWrites=true&w=majority"
     // DB:'mongodb://localhost/clinicals' //if run on aws linux box
 }

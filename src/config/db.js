a6e97af745863cc1b478a394f2626d926808a961
yujing <yujing>
2023-10-20 02:29:59 +0800

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,11 @@
 import mongoose from 'mongoose';
 import props from './properties'
 module.exports = function(){
-    mongoose.connect(props.DB)
+    mongoose.connect(props.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
 }
